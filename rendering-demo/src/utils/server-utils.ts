392decb03208b15aa59bbe8d7066e2b66df58575
Aniebiet Edet <Ahnibaba>
2025-07-02 15:44:17 +0100

    rendering-demo

---
+++ rendering-demo/src/utils/server-utils.ts
@@ -0,0 +1,8 @@
+import "server-only"
+export const serverSideFunction = () => {
+    console.log(`use multiple libraries, use environment variables,
+        interact with a database process confidentials information`
+    );
+    return "server result"
+    
+}
